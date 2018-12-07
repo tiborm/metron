e0c9d6bfc5580c180e0815beb576abbb15baaa5e
Shane Ardell <sardell@hortonworks.com>
2018-12-07 14:43:51 +0100

    add and edit description for parser groups

--- metron-interface/metron-config/src/app/sensors/sensor-parser-list/sensor-parser-list.component.spec.ts
+++ metron-interface/metron-config/src/app/sensors/sensor-parser-list/sensor-parser-list.component.spec.ts
@@ -774,7 +774,7 @@ describe('Component: SensorParserList', () => {
     expect(component.isDeletedOrPhantom(sensor)).toBe(true);
   }));
 
-  fit('should hide parser controls when they cannot be used', async(() => {
+  it('should hide parser controls when they cannot be used', async(() => {
     fixture.detectChanges();
 
     const stopButtons = fixture.debugElement.queryAll(By.css('[data-qe-id="stop-parser-button"]'));
