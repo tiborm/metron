2c91fa5e29d43c96be5562be7cdf6399c8f85b1f
tiborm <tibor.meller@gmail.com>
2018-12-12 13:10:46 +0100

    [context-menu] feeding dynamic menu items from a service fake

--- metron-interface/metron-alerts/src/app/shared/context-menu/context-menu.service.ts
+++ metron-interface/metron-alerts/src/app/shared/context-menu/context-menu.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class ContextMenuService {
+
+  getConfig(): Observable<{}> {
+    // TODO this here is the actual API definition
+    return of({
+      id: [{ label: 'id: dynamic menu item 01', urlPattern: '/{}'}],
+      ip_src_addr: [{ label: 'ip_src_addr: dynamic menu item 02', urlPattern: '/{}'}],
+      host: [{ label: 'host: dynamic menu item 01', urlPattern: '/{}'}],
+    })
+  }
 
   constructor(private http: HttpClient) { }
 }
