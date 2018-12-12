0731f0f6a78c57d9988b2827c63e340d1599ab07
tiborm <tibor.meller@gmail.com>
2018-12-12 13:10:46 +0100

    [context-menu] feeding dynamic menu items from a service fake

--- metron-interface/metron-alerts/src/app/shared/context-menu/context-menu.component.ts
+++ metron-interface/metron-alerts/src/app/shared/context-menu/context-menu.component.ts
@@ -1,15 +1,48 @@
-import { Component, ContentChild, AfterContentInit, OnDestroy, ViewChild, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
+import { Component, AfterContentInit, OnDestroy, ViewChild, ElementRef, Renderer2, Input, OnInit } from '@angular/core';
 import { ContextMenuService } from './context-menu.service';
-import { Subscriber, Subscription, fromEvent, Subject } from 'rxjs';
+import { fromEvent, Subject, Observable } from 'rxjs';
 import Popper from 'popper.js';
-import { takeUntil, filter } from 'rxjs/operators';
+import { takeUntil, map } from 'rxjs/operators';
+
+// TODO: extract this class
+export class DynamicMenuItem {
+
+  label: string;
+  private urlPattern: string;
+
+  constructor(readonly config: { label: string, urlPattern: string }) {
+    if (this.isValid(config)) {
+      this.label = config.label;
+      this.urlPattern = config.urlPattern;
+    };
+  }
+
+  get url() {
+    return this.urlPattern
+  }
+
+  /**
+   * Validating server respons and logging error if something required missing.
+   *
+   * @param config {} Menu config object received from and endpoint.
+   */
+  private isValid(config: {}) {
+    return ['label', 'urlPattern'].every((requiredField) => {
+      if (config.hasOwnProperty(requiredField)) {
+        return true;
+      } else {
+        console.error(`[context-menu] Service returned with a incomplete config object. Missing field: ${requiredField}`);
+      }
+    })
+  }
+}
 
 @Component({
   selector: '[withContextMenu]',
   templateUrl: './context-menu.component.html',
   styleUrls: ['./context-menu.component.scss']
 })
-export class ContextMenuComponent implements  AfterContentInit, OnDestroy {
+export class ContextMenuComponent implements OnInit, AfterContentInit, OnDestroy {
 
   @ViewChild('dropDown') dropDown: ElementRef;
   @ViewChild('clickOutsideCanvas') outside: ElementRef;
@@ -17,7 +50,9 @@ export class ContextMenuComponent implements  AfterContentInit, OnDestroy {
   @Input() predefinedItems: { label: string, event: string }[];
   @Input() menuTitle: string;
   @Input() menuConfigId: string;
+
   @Input() data: any;
+  dynamicMenuItems: DynamicMenuItem[] = [];
 
   private destroyed$: Subject<boolean> = new Subject<boolean>();
 
@@ -29,8 +64,22 @@ export class ContextMenuComponent implements  AfterContentInit, OnDestroy {
     private renderer: Renderer2
     ) {}
 
+  ngOnInit() {
+    this.fetchContextMenuConfig();
+  }
+
   ngAfterContentInit() {
     this.subscribeTo();
+  }
+
+  private fetchContextMenuConfig() {
+    this.contextMenuSvc.getConfig()
+      .pipe(map((allConfigs: {}) => allConfigs[this.menuConfigId]))
+      .subscribe((config: { label: string, urlPattern: string }[]) => {
+        this.dynamicMenuItems = config ? config.map(
+          menuConfig => new DynamicMenuItem(menuConfig)
+        ) : []
+      });
   }
 
   private subscribeTo() {
@@ -67,6 +116,10 @@ export class ContextMenuComponent implements  AfterContentInit, OnDestroy {
   dispatchMenuEvent(event: string) {
     this.close();
     this.el.nativeElement.dispatchEvent(new CustomEvent(event));
+  }
+
+  dynamicItemClicked($event: MouseEvent, url: string) {
+
   }
 
   ngOnDestroy() {
--- metron-interface/metron-alerts/src/app/shared/context-menu/readme.md
+++ metron-interface/metron-alerts/src/app/shared/context-menu/readme.md
@@ -1,2 +1,11 @@
 https://hortonworks.jira.com/browse/BUG-114327
 
+Context menus
+- on value (dynamic column)
+- on row
+- on score (fixed column)
+- on value inside meta alert (dynamic column)
+- on row inside meta alert
+- on score inside meta alert (fixed column)
+- id on meta alert (fixed column)
+- score on meta alert (fixed column)
