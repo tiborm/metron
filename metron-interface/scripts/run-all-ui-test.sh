8e4f4af1f4419f448c8091f55b18184f50f7659e
tiborm <tibor.meller@gmail.com>
2019-02-27 14:43:23 +0100

    [cypress] navigating to the proper folder

--- metron-interface/scripts/run-all-ui-test.sh
+++ metron-interface/scripts/run-all-ui-test.sh
@@ -15,6 +15,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$current_path/.."
 
 echo ""
 echo "      _/_/    _/                        _/                    _/    _/  _/_/_/   "
