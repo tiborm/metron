47e03d64750d1ae644a01a1ac3aa149fc1dca318
tiborm <tibor.meller@gmail.com>
2019-03-05 13:15:05 +0100

    METRON-2023 adding set -e to the run script to fail fast

--- metron-interface/metron-alerts/scripts/run-all-test.sh
+++ metron-interface/metron-alerts/scripts/run-all-test.sh
@@ -15,6 +15,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+set -e
+
 current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$current_path/.."
 
