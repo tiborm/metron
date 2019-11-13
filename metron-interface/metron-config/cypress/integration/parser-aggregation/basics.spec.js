4e63935bce431775c4574179ef98fd7a287874df
Tibor Meller <tibor.meller@gmail.com>
2019-11-13 17:40:12 +0100

    METRON-2296 Adding more tests, cleanup

--- metron-interface/metron-config/cypress/integration/parser-aggregation/basics.spec.js
+++ metron-interface/metron-config/cypress/integration/parser-aggregation/basics.spec.js
@@ -16,7 +16,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-describe('PCAP Tab', () => {
+describe('Parser Aggregation Basics', () => {
+
+  const GROUPED_PARSER_MARKER = '---';
+
+  const PARSER_IDS = {
+    DEFAULT_GROUP: 'bro__snort__yaf',
+    BRO: 'bro',
+    SNORT: 'snort',
+    YAF: 'yaf'
+  }
 
   const triggerEventOn = (parserName, event) => {
     cy.get(`[data-qe-id="parser-row-${parserName}"]`).then(el => {
@@ -46,6 +55,37 @@ describe('PCAP Tab', () => {
     triggerEventOn(parserName, new MouseEvent('drop'));
   }
 
+  const assertConfirmMessageAppears = confirmMsg => {
+    cy.contains(confirmMsg).should((elements) => {
+      expect(elements[0].parentElement.classList.contains('alert-success')).to.eq(true);
+      expect(elements[0].innerText).to.eq(confirmMsg);
+    });
+  }
+
+  const assertIsGroup = (parserId, assertTo) => {
+    cy.log(`Asserting ${parserId} is ${ !assertTo ? 'not' : '' } in a group...`);
+
+    cy.get(`[data-qe-id="parser-row-${parserId}"] td:nth-of-type(2)`).should(rows => {
+      if (assertTo) {
+        expect(rows[0].firstElementChild.innerText.trim()).to.eq(GROUPED_PARSER_MARKER);
+      } else {
+        expect(rows[0].firstElementChild.innerText.trim()).to.not.eq(GROUPED_PARSER_MARKER);
+      }
+    });
+  }
+
+  const assertMarkedAsDeleted = (parserId, assertTo) => {
+    cy.log(`Asserting ${parserId} is ${ !assertTo ? 'not' : '' } deleted`)
+    cy.get(`[data-qe-id="parser-row-${parserId}"]`).should(rows => {
+      expect(rows[0].classList.contains('deleted')).to.eq(assertTo);
+    });
+  }
+
+  const assertListContains = (parserId, assertTo) => {
+    const matcher = assertTo ? 'be.visible' : 'not.be.visible';
+    cy.get(`[data-qe-id="parser-row-${parserId}"]`).should(matcher);
+  }
+
   beforeEach(() => {
     cy.server();
     cy.route({
@@ -67,48 +107,109 @@ describe('PCAP Tab', () => {
     cy.contains('LOG IN').click();
   });
 
-  // it('Parser aggregate creation', () => {
-  //   startDrag('snort');
-  //   dropOn('bro');
-  // });
-
   it('Stopping bro__snort__yaf group', () => {
-    cy.get('[data-qe-id*="parser-row"]:nth-of-type(1) [data-qe-id="stop-parser-button"]').click();
-    cy.contains('[data-qe-id*="parser-row"]:nth-of-type(1) td:nth-of-type(4)', 'Stopped');
+    cy.get(`[data-qe-id="parser-row-${PARSER_IDS.DEFAULT_GROUP}"] [data-qe-id="stop-parser-button"]`).click();
+
+    assertConfirmMessageAppears('Stopped sensor bro__snort__yaf')
+
+    cy.contains(`[data-qe-id="parser-row-${PARSER_IDS.DEFAULT_GROUP}"] td:nth-of-type(4)`, 'Stopped')
+      .then(elements => expect(elements[0].innerText).to.eq('Stopped'));
   });
 
 
   it('Disassembling bro__snort__yaf group', () => {
-    cy.log('Dragging BRO parser out from bro__snort__yaf');
+    assertIsGroup(PARSER_IDS.BRO, true);
+    assertIsGroup(PARSER_IDS.SNORT, true);
+    assertIsGroup(PARSER_IDS.YAF, true);
+    
     startDrag('bro');
     dragOver('jsonMapQuery', 'after');
     dropOn('jsonMapQuery');
+    
+    assertIsGroup(PARSER_IDS.BRO, false);
 
     startDrag('snort');
     dragOver('jsonMapQuery', 'after');
     dropOn('jsonMapQuery');
+    
+    assertIsGroup(PARSER_IDS.SNORT, false);
 
     startDrag('yaf');
     dragOver('jsonMapQuery', 'after');
     dropOn('jsonMapQuery');
+    
+    assertIsGroup(PARSER_IDS.YAF, false);
 
-    cy.contains('APPLY').click();
-    cy.wait(5000);
+    cy.contains('button', 'APPLY').click();
 
+    assertConfirmMessageAppears('Your changes has been applied successfully');
+    
     cy.route('GET', '/api/v1/sensor/parser/config').as('getParserConfigs');
     cy.route('GET', '/api/v1/sensor/parser/group').as('getParserGroups');
 
     cy.reload();
 
     cy.log('Asserting parser/config API response');
-    cy.wait('@getParserConfigs').then((xhr) => {
-      // FIXME this type of assertation not works
-      cy.wrap(xhr.response.body.bro.group, ).should('be', 'kaka');
-      cy.wrap(xhr.response.body.yaf.group, ).should('be', '');
-      cy.wrap(xhr.response.body.snort.group, ).should('be', '');
-      // debugger;
-    })
+    cy.wait('@getParserConfigs').should((xhr) => {
+      const parserConfigs = xhr.response.body;
+      
+      expect(parserConfigs.bro.group).to.eq('');
+      expect(parserConfigs.yaf.group).to.eq('');
+      expect(parserConfigs.snort.group).to.eq('');
+    });
 
+    assertIsGroup(PARSER_IDS.BRO, false);
+    assertIsGroup(PARSER_IDS.SNORT, false);
+    assertIsGroup(PARSER_IDS.YAF, false);
   });
 
+  it('Delete default bro__snort__yaf gropu', () => {
+    cy.get(`[data-qe-id="parser-row-${PARSER_IDS.DEFAULT_GROUP}"] [data-qe-id="delete-parser-button"]`).click();
+
+    // Asserting confirmation dialogue shows up
+    cy.contains('Are you sure you want to delete bro__snort__yaf').should('be.visible');
+    cy.wait(500);
+    cy.contains('button', 'OK').click();
+
+    assertMarkedAsDeleted(PARSER_IDS.DEFAULT_GROUP, true);
+
+    cy.route('GET', '/api/v1/sensor/parser/group').as('getParserGroups');
+    cy.contains('button', 'APPLY').click();
+    cy.reload();
+    cy.wait('@getParserGroups');
+
+    assertListContains(PARSER_IDS.DEFAULT_GROUP, false);
+  });
+
+  it('Parser aggregate creation', () => {
+    const testGroupName = 'Cypress-test-group';
+
+    startDrag('snort');
+    dropOn('bro');
+
+    cy.get('input[type="text"]').first().clear().type(testGroupName);
+    cy.get('input[type="text"]').last().type('Cypress-test-group-DESC');
+
+    cy.contains('button', 'DONE').click();
+
+    startDrag('yaf');
+    dropOn(testGroupName);
+
+    cy.route('GET', '/api/v1/sensor/parser/group').as('getParserGroups');
+    cy.contains('button', 'APPLY').click();
+    cy.reload();
+    cy.wait('@getParserGroups');
+
+    assertListContains(testGroupName, true);
+  });
+
+
+  /**
+   * OTHER TESTABLES
+   * 
+   * - enable disable group
+   * - enadble disable parser
+   * 
+   * - moving parser from an existing group to another (running too)
+   */
 });
